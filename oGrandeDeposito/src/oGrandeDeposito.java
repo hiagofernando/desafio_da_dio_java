9978a16873e6bf9e283e264ad237f39db7ec1e39
hiago fernando <hiago30fernando@hotmail.com>
2023-09-01 22:56:47 -0300

    Dia 01/09 concluido desafios e organizados outros

--- oGrandeDeposito/src/oGrandeDeposito.java
+++ oGrandeDeposito/src/oGrandeDeposito.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class oGrandeDeposito {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
 
         double valor = scanner.nextDouble();
 
         if (valor > 0) {
-            //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
-        } else if (valor == 0) {
-            //TODO: Imprimir a mensagem de valor inválido.
+            System.out.println("Deposito realizado com sucesso!\r\n" +
+                    "Saldo atual: R$ " + df.format(valor));
+
+        } else if (valor < 0) {
+            System.out.print("Valor invalido! Digite um valor maior que zero.");
+
         } else {
-             //TODO: Imprimir a mensagem de encerrar o programa.
+            System.out.print("Encerrando o programa...");
         }
     }
 }
